c5aa67089060f7911d63db7e23ea7ba04cd71eb0
Devarsh Patel <devarshptl@Devarshs-MacBook-Pro.local>
2024-03-27 19:32:25 -0500

    Enable auto-setup for mason-lspconfig

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,61 +1,54 @@
--- Language server config
+-- Language server configurations
+
+-- LSP
+local lsp_packages = {
+  "lua_ls",        -- Lua
+  "clangd",        -- C/C++
+  "tsserver",      -- Javascript/TypeScript
+  "pyright",       -- Python
+  "rust_analyzer", -- Rust
+  "volar",         -- Vue
+}
+
+local function lsp_setup(lspconfig, lsp_list, setup_obj)
+  for _, lsp in pairs(lsp_list) do
+    lspconfig[lsp].setup(setup_obj)
+  end
+end
+
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				-- Ensure language servers are installed
-				-- Note: Don't forget to add this servers in the config below
-				ensure_installed = {
-					-- LSP
-					"lua_ls", -- Lua
-					"clangd", -- C/C++
-					"tsserver", -- Javascript/TypeScript
-					"pyright", -- Python
-					"rust_analyzer", -- Rust
-					"volar", -- Vue
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = { "hrsh7th/cmp-nvim-lsp" },
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    { "hrsh7th/cmp-nvim-lsp" },
+    {
+      "williamboman/mason.nvim",
+      config = function()
+        require("mason").setup({})
+      end,
+    },
+    {
+      "williamboman/mason-lspconfig.nvim",
+      config = function()
+        require("mason-lspconfig").setup({
+          -- Ensure language servers are installed
+          ensure_installed = lsp_packages,
+        })
+      end,
+    },
+  },
+  config = function()
+    local lspconfig = require("lspconfig")
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			-- Setup required language servers
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.rust_analyzer.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.volar.setup({
-				capabilities = capabilities,
-			})
+    -- Setup required language servers
+    lsp_setup(lspconfig, lsp_packages, {
+      capabilities = capabilities,
+    })
 
-			-- key bindings
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+    -- key bindings
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+  end,
 }
