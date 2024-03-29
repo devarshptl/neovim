8645dbb7bb723e888ac46859a828a96c8267f1b0
Devarsh Patel <devarshptl@Devarshs-MacBook-Pro.local>
2024-03-29 15:42:28 -0500

    Setup nvim-treesitter-context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,19 @@
 -- Treesitter
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
+  "nvim-treesitter/nvim-treesitter-context",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
   config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
+    require("treesitter-context").setup({})
   end,
 }
