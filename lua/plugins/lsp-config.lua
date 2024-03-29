da795c64f42f282df45143bef0557e659bb6c343
Devarsh Patel <devarshptl@Devarshs-MacBook-Pro.local>
2024-03-29 16:07:44 -0500

    Use rustaceanvim for rust, provide support for go using gopls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,54 +2,62 @@
 
 -- LSP
 local lsp_packages = {
-  "lua_ls",        -- Lua
-  "clangd",        -- C/C++
-  "tsserver",      -- Javascript/TypeScript
-  "pyright",       -- Python
-  "rust_analyzer", -- Rust
-  "volar",         -- Vue
-  "angularls",     -- Angular
+  "lua_ls",    -- Lua
+  "clangd",    -- C/C++
+  "tsserver",  -- Javascript/TypeScript
+  "pyright",   -- Python
+  "gopls",     -- Go
+  "volar",     -- Vue
+  "angularls", -- Angular
 }
 
 return {
-  "williamboman/mason-lspconfig.nvim",
-  dependencies = {
-    {
-      "williamboman/mason.nvim",
-      config = function()
-        require("mason").setup({})
-      end
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        config = function()
+          require("mason").setup({})
+        end
+      },
+      { "hrsh7th/cmp-nvim-lsp" },
+      {
+        "neovim/nvim-lspconfig",
+      },
     },
-    { "hrsh7th/cmp-nvim-lsp" },
-    {
-      "neovim/nvim-lspconfig",
-    },
-  },
-  config = function()
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
-    local lspconfig = require("lspconfig")
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
 
-    require("mason-lspconfig").setup({
-      ensure_installed = lsp_packages,
-      handlers = {
-        function(server_name)
-          lspconfig[server_name].setup({
-            capabilities = capabilities,
-          })
-        end,
-        ["lua_ls"] = function()
-          lspconfig.lua_ls.setup({
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim" }
+      require("mason-lspconfig").setup({
+        ensure_installed = lsp_packages,
+        handlers = {
+          function(server_name)
+            lspconfig[server_name].setup({
+              capabilities = capabilities,
+            })
+          end,
+          ["lua_ls"] = function()
+            lspconfig.lua_ls.setup({
+              capabilities = capabilities,
+              settings = {
+                Lua = {
+                  diagnostics = {
+                    globals = { "vim" }
+                  }
                 }
               }
-            }
-          })
-        end,
-      },
-    })
-  end,
+            })
+          end,
+        },
+      })
+    end,
+  },
+  -- Use dedicated rust plugin
+  {
+    "mrcjkb/rustaceanvim",
+    version = '^4', -- Recommended
+    ft = { 'rust' },
+  },
 }
