fa147ec6be46f4acf82c2db6bb5f072b85f9b5fc
Devarsh Patel <devarshptl@Devarshs-MacBook-Pro.local>
2024-03-30 19:06:17 -0500

    Enable autoreload neo-tree on change

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,13 +9,11 @@ return {
 	},
 	config = function()
 		require("neo-tree").setup({
+			use_libuv_file_watcher = true,
 			event_handlers = {
 				{
 					event = "file_opened",
 					handler = function(file_path)
-						-- auto close
-						-- vimc.cmd("Neotree close")
-						-- OR
 						require("neo-tree.command").execute({ action = "close" })
 					end,
 				},
